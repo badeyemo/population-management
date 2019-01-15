a9fc6a61f4917c3e155e6ef66f2d228aedc3e677
ludralph <raphaelumoh@gmail.com>
2019-01-15 21:11:59 +0100

    ch: update location model

--- models/location.js
+++ models/location.js
@@ -1,10 +1,48 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
-  const Location = sequelize.define('Location', {
-    locationName: DataTypes.STRING
-  }, {});
-  Location.associate = function(models) {
-    // associations can be defined here
-  };
-  return Location;
-};
+  const Location = sequelize.define(
+    'Location',
+    {
+      locationName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          notEmpty: {
+            args: true,
+            msg: 'Name can not be empty'
+          }
+        }
+      },
+      male: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+        validate: {
+          notEmpty: {
+            args: true,
+            msg: 'Male value cannot be empty'
+          }
+        }
+      },
+      female: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+        validate: {
+          notEmpty: {
+            args: true,
+            msg: 'Female value cannot be empty'
+          }
+        }
+      }
+    },
+    {}
+  )
+  Locations.associate = models => {
+    Locations.hasMany(
+      models.Locations,
+      {as: 'childLocation', foreignKey: 'parentLocationId'},
+      {onDelete: 'set null', hooks: true}
+    )
+  }
+  return Locations
+}
