079fb87638efa8810182be076be37716d5f30b2c
ludralph <raphaelumoh@gmail.com>
2019-01-17 12:30:41 +0100

    add engine and update port

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
   });
 });
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.port || 5000;
 
 app.listen(PORT, () => {
   console.log(`App started on port ${PORT}`);
